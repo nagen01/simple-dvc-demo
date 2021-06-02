850e79c85200dc9035aec2fc82c357f1ac716592
nagen01 <nagendrabahadursingh5005@gmail.com>
2021-06-02 11:37:17 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="It's wineq package",
+    author="nagen01",
+    packages=find_packages(),
+    licence="MIT"
+)
